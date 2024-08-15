ba41d934533bb176d798c4bb19ff10a5b291124a
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-08-15 00:10:12 -0400

    complete exercise 2

--- challenges.js
+++ challenges.js
@@ -46,6 +46,7 @@ and the rating for Bob's challenge is the triplet b = [b[0], b[1], b[2]].
 
 Your task is to find their comparison points by comparing a[0] with b[0], 
 a[1] with b[1], and a[2] with b[2].
+..
 
 If a[i] > b[i], then Alice is awarded 1 point.
 If a[i] < b[i], then Bob is awarded 1 point.
@@ -64,8 +65,14 @@ compareTriplets([17, 28, 30], [99, 16, 8]) //=> [2, 1]
 -----------------------------------------------------------------------------*/
 // Your solution for 02- here:
 
-
-
+function compareTriplets(a, b) {
+  let sums = [0, 0]
+  for(let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) sums[0]++
+    else if (a[i] < b[i]) sums[1]++
+  } 
+  return sums
+}
 /*-----------------------------------------------------------------------------
 
 Challenge: 03 - diagonalDifference
