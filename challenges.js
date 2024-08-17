a32ddc3c4d9580873f9012c2dc28b3a22a7fb2df
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-08-17 09:15:50 -0400

    complete exercise 3

--- challenges.js
+++ challenges.js
@@ -99,7 +99,11 @@ diagonalDifference([[11,2,4], [4,5,6], [10,8,-12]]) // => 15
 -----------------------------------------------------------------------------*/
 // Your solution for 03- here:
 
-
+function diagonalDifference(arr) {
+  let leftDiagonal = arr[0][0] + arr[1][1] + arr[2][2]
+  let rightDiagonal = arr[0][2] + arr[1][1] + arr[2][0]
+  return Math.abs(leftDiagonal - rightDiagonal)
+}
 
 /*-----------------------------------------------------------------------------
 Challenge: 04 - plusMinus
